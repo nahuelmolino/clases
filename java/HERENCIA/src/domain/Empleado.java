93508205379d633e77fd32c6079bfd0dc42253af
nahuel.molino.mza@gmail.com <nahuel.molino.mza@gmail.com>
2024-10-31 16:13:00 -0300

    clase 9, video 3

--- java/HERENCIA/src/domain/Empleado.java
+++ java/HERENCIA/src/domain/Empleado.java
@@ -3,6 +3,43 @@ package domain;
 
 //hija de Persona.
 public class Empleado extends Persona {
+    private int idEmpleado;
+    private double sueldo;
+    private static int contadorEmpleados;// Es para incrementar.
+    
+   //Constructores
+    public Empleado(String nombre, double sueldo){
+        super(nombre);
+        this.idEmpleado= ++Empleado.contadorEmpleados;
+        this.sueldo= sueldo;
+    }
 
+    public int getIdEmpleado() {
+        return this.idEmpleado;
+    }
+
+    public double getSueldo() {
+        return this.sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        //Es mas eficiente que la concatenacion comun. El operador + es mucho mas lento.
+        //crea objeto StringBuilder
+        StringBuilder sb = new StringBuilder();
+        //Metodo append, 
+        sb.append("Empleado{");
+        sb.append("idEmpleado=").append(idEmpleado);
+        sb.append(", sueldo=").append(sueldo);
+        sb.append('}');
+        // para devolver un string, usamos el metodo toString() del sb ya que sino no son compatibles.
+        return sb.toString();
+    }
+    
+    
     
 }
